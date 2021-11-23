d2a4a4c210511b8332e6bf432bff6e107130b974
karthickraja05 <karthickraja.gm@gmail.com>
2021-11-23 23:41:54 +0530

    Base Structute For Vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueRouter,VueAxios,axios);
+
+const app = new Vue(Vue.util.extend(App)).$mount('#app');
+
